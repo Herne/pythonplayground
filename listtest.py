8089226eea4c19cbe86a91ee3bcd6cdbfa952587
Herne Liu <hxliu@advent.com>
2017-04-05 14:05:05 +0800

    04/05/2017 LP5e C4 List

--- listtest.py
+++ listtest.py
@@ -1,2 +1,11 @@
-L = [123,'spam', '1.23']
-print(len(L))
+M = [[1,2,3],[4,5,6],[7,8,9]]
+print('M[1] = ' + str(M[1]))
+print('M[1][2] = ' + str(M[1][2]))
+print('col2 = [row[1] for row in M] = ' + str([row[1] for row in M]))
+print('M = ' + str(M))
+print('[row[1] for row in M if row[1] % 2 == 0] = ' + str([row[1] for row in M if row[1] % 2 == 0]))
+print('diag = [M[i][i] for i in [0,1,2]] = ' + str([M[i][i] for i in [0,1,2]]))
+print('doubles = [c * 2 for c in "spam"] = ' + str([c * 2 for c in "spam"]))
+print('list(range(4)) = ' + str(list(range(4))))
+print('list(range(-6, 7, 2)) = ' + str(list(range(-6, 7, 2))))
+
