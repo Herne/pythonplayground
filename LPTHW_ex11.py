a4fda65076a336ec9b90f5468378acb85f33ce4a
Herne <liuyue3000@gmail.com>
2017-04-11 23:33:16 +0800

    ex11

---
+++ LPTHW_ex11.py
@@ -0,0 +1,15 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+s = raw_input('--->')
+print s
+
+car = raw_input('What\'s the make of your first car? ')
+pet = raw_input('Are you are cat person or a dog person? ')
+print "So, your first car was a %s, and you are a %s." % (car, pet)
