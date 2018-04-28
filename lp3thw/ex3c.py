0fa9ff469e77fb646dd6b8f28cdc17d9e1e4799a
Yue Herne Liu <liuyue3000@gmail.com>
2018-04-28 20:51:44 +0800

    exercise 3 study drills

---
+++ lp3thw/ex3c.py
@@ -0,0 +1,3 @@
+# Calculate credit card bill 
+print ("April 28 Credit Card Bill")
+print (100 + 200 + 300 + 400)
---
+++ lp3thw/ex3d.py
@@ -0,0 +1,28 @@
+# prompt
+print ("I will now count my chickens:")
+# 30 / 6 float 5.0
+print ("Hens", 25.0 + 30.0 / 6.0) 
+# 75 mod 4 = 18 (3)
+print ("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+# prompt
+print ("Now I will count the eggs:") 
+# 4 % 2 = 0, 1 / 4 = float 0.25
+print (3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+# prompt 
+print ("Is is true that 3 + 2 < 5 - 7?")
+# 5 < -2 is false
+print (3.0 + 2.0 < 5.0 - 7.0)  
+# 5 
+print ("What is 3 + 2?", 3.0 + 2.0)
+# -2
+print ("What is 5 - 7?", 5.0 - 7.0)
+# prompt 
+print ("Oh, that's why it's False.") 
+# prompt 
+print ("How about some more.")
+# 5 greater than -2 is true 
+print ("Is it greater?", 5.0 > -2.0) 
+# 5 greater than equal -2 is true
+print ("Is it greater or equal?", 5.0 >= -2.0)
+# 5 less than equal -2 is false
+print ("Is it less or euqal?", 5.0 <= -2.0)
