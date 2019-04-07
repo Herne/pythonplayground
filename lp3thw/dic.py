2c014b08555fea831abbab2eaec06b5d25599c7d
Yue Herne Liu <liuyue3000@gmail.com>
2019-04-07 17:58:32 +0800

    pluralsight 4/7

---
+++ lp3thw/dic.py
@@ -0,0 +1,11 @@
+student = {
+    "name": "Mark",
+    "student_id": 15163,
+    "feedback": None
+}
+
+print(student["name"])
+print(student.get("name", "student not found"))
+print(student.keys())
+print(student.values())
+print(student["lastname"])˝˝
---
+++ lp3thw/loops.py
@@ -0,0 +1,7 @@
+student_name = ["James", "Katarina", "Jessica", "Mark", "Bort", "Frank Grimes", "Max Power"]
+
+for name in student_name:
+    if name == "Bort":
+        continue
+        print("Found him! " + name) 
+    print("Currently testing " + name)
