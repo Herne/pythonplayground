87d27de92c7e09dc1269bea574bb135e666499f9
herne <liuyue3000@gmail.com>
2020-02-16 00:02:02 +0800

    test pyCharm

--- lp3thw/ex3.py
+++ lp3thw/ex3.py
@@ -1,28 +1,28 @@
 # prompt
-print ("I will now count my chickens:")
+print("I will now count my chickens:")
 # 30 / 6 float 5.0
-print ("Hens", 25 + 30 / 6) 
+print("Hens", 25 + 30 / 6)
 # 75 mod 4 = 18 (3)
-print ("Roosters", 100 - 25 * 3 % 4)
+print("Roosters", 100 - 25 * 3 % 4)
 # prompt
-print ("Now I will count the eggs:") 
+print("Now I will count the eggs:")
 # 4 % 2 = 0, 1 / 4 = float 0.25
-print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 # prompt 
-print ("Is is true that 3 + 2 < 5 - 7?")
+print("Is is true that 3 + 2 < 5 - 7?")
 # 5 < -2 is false
-print (3 + 2 < 5 - 7)  
+print(3 + 2 < 5 - 7)
 # 5 
-print ("What is 3 + 2?", 3 + 2)
+print("What is 3 + 2?", 3 + 2)
 # -2
-print ("What is 5 - 7?", 5 - 7)
+print("What is 5 - 7?", 5 - 7)
 # prompt 
-print ("Oh, that's why it's False.") 
+print("Oh, that's why it's False.")
 # prompt 
-print ("How about some more.")
-# 5 greater than -2 is true 
-print ("Is it greater?", 5 > -2) 
+print("How about some more.")
+# 5 greater than -2 is true
+print("Is it greater?", 5 > -2)
 # 5 greater than equal -2 is true
-print ("Is it greater or equal?", 5 >= -2)
+print("Is it greater or equal?", 5 >= -2)
 # 5 less than equal -2 is false
-print ("Is it less or euqal?", 5 <= -2)
+print("Is it less or equal?", 5 <= -2)
