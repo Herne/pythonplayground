1a9b744a6fa979601fe211b320c0b399d14d5e9d
herne <liuyue3000@gmail.com>
2020-03-22 01:02:48 +0800

    test pyCharm

---
+++ lp3thw/ex40.py
@@ -0,0 +1,25 @@
+class Song(object):
+
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+
+happy_bday = Song(["Happy birthday to you",
+                   "I don't want to get sued",
+                   "So I'll stop right there"])
+
+bulls_on_parade = Song(["They rally around tha family",
+                        "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
+
+lyrics = ["你永远不懂我伤悲", "像白天不懂夜的黑"]
+herne = Song(lyrics)
+herne.sing_me_a_song()
+
